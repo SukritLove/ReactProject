b51b9c3c0723903b5f1882028f42aee41a97c7bf
Sukrit Rerk-u-cote <sukrit_love@Sukrits-MacBook-Pro.local>
2023-07-01 17:41:43 +0700

    CatJs

---
+++ components/Cat.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Cat = () => {
+  return (
+    <View>
+      <Text>Hello, I am your Cat!</Text>
+    </View>
+  );
+};
+
+export default Cat;
