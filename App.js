55d842503f0d23e496427ba25588475e00e31d1c
Sukrit Rerk-u-cote <sukrit_love@Sukrits-MacBook-Pro.local>
2023-07-15 12:08:15 +0700

    Date[15July2023]

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Cat from "./components/Cat";
-import CatDetail from "./components/CatDetail";
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+// import Cat from './components/Cat'
+// import CatDetail from './components/CatDetail';
+// import ViewBoxesWithColorAndText from './components/ViewBoxesWithColorAndText';
+// import DisplayandImage from './components/DisplayandImage';
+// import LotsOfGreeting from './components/LotsOfGreeting';
+import CustomText from './components/CustomText';
 
 export default function App() {
   return (
-    <View>
-      <Cat />
-      <CatDetail />
+    <View style={styles.container}>
+      {/* <Cat/>
+      <CatDetail/>
+
+      <ViewBoxesWithColorAndText/>
+
+      <DisplayandImage/>
+     
+      <LotsOfGreeting/> */}
+
+      <CustomText/>
     </View>
   );
-  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
