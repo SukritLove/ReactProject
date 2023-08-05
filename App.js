de4473ca9c3f7750b32ca0585bd3e4383dd733a5
Sukrit Rerk-u-cote <sukrit_love@Sukrits-MacBook-Pro.local>
2023-08-05 09:23:51 +0700

    Recap Touchable [Week6-August-05-2023]

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 
 import { StyleSheet, Text, View } from 'react-native';
-import Recap from './components/Recap';
+import TouchablePractice from './components/TouchablePractice';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Recap/>
+      <TouchablePractice/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 });
