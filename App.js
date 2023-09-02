0c4b84afd62e74295c1bac2fffff6a389bd817a6
Sukrit Rerk-u-cote <sukrit_love@Sukrits-MacBook-Pro.local>
2023-09-02 10:27:52 +0700

    First Commit [Lecture9-September-02-2023]

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
-import FlastListAPI from "./components/FlatListAPI";
-import News from "./components/News";
-import ProductScreen from "./components/ProductScreen";
+import { Text, View, Button } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./Screen/HomeScreen";
+import DetailsScreen from "./Screen/DetailsScreen";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      {/* <FlastListAPI/>
-      <News/> */}
-<ProductScreen/>
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "OverView" }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop:30,
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-});
+export default App;
